415f8cee6f5ae4a9e21b63c2a6f6c3b4dcb6a986
AkshitaGaur-hub <gaurakshita407@gmail.com>
2026-07-12 09:29:35 +0530

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <p className='font-bold'>Hello</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/Frontend/employee/EmployeeRouter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EmployeeRouter = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default EmployeeRouter
